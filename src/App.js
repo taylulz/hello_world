c0c723ed5a5eddd79805082c2736b4e7b87226cb
TayLulz <taylulzcode@gmail.com>
2021-05-23 11:24:41 -0700

    bootstrap react app, remove formatting

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +16,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+
+    </>
   );
 }
 
